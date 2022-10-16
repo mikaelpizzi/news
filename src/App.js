7d86d7356437c45521815b368188fa4c7f4d3834
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-10-15 23:37:30 -0400

    [ADD] Consult API and read results

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Fragment, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import Form from "./components/Form";
 import Header from "./components/Header";
 
@@ -6,6 +6,20 @@ function App() {
 
   // Define topic and news
   const [ topic, saveTopic ] = useState('');
+  const [ news, saveNews ] = useState([]);
+
+  useEffect(() => {
+    const consultAPI = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=us&category=${topic}&apiKey=aa8606bb88bd47ff8a3c548a1cd46981`
+
+      const response = await fetch(url);
+      const news = await response.json();
+
+      saveNews(news.articles);
+    }
+
+    consultAPI();
+  }, [topic])
   return (
     <Fragment>
       <Header 
